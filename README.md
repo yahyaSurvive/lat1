0e482066b6fb778d42002c200be36bacf3fcc840
yahyaSurvive <putuyahya5566@gmail.com>
2022-01-01 19:28:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-hvzflq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-hvzflq)
---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('button').click(function () {
+    $('h1').hide();
+  });
+});
+//ini adalah latihan
+//nama yahya ganteng
