4b9530b6f54d1916cf4cbbf83ad905fcca593f73
yahyaSurvive <putuyahya5566@gmail.com>
2022-01-02 11:44:56 +0800

    2 files modified

--- script.js
+++ script.js
@@ -11,3 +11,66 @@
 // a.addEventListener('click', function () {
 //   document.body.classList.toggle('lampu');
 // });
+
+//manipulasi dom pada javacript
+//document.createElement()
+//document.createTextNode()
+//node.appendChild()
+//node.insertBefore()
+//parentNode.removeChild()
+//parentNode.replaceChild()
+
+// const pBaru = document.createElement('p');
+// const textBaru = document.createTextNode('Yahya Ne');
+
+//gabungkan
+// pBaru.appendChild(textBaru);
+
+//tentukanTempat
+// const tempat = document.getElementById('a');
+// tempat.appendChild(pBaru);
+
+// const a = document.getElementsByTagName('button')[0];
+// a.addEventListener('click', function () {
+//   const nama = document.getElementsByTagName('input')[0];
+//   alert('Nama saya : ' + nama.value);
+// });
+
+// const klik = document.getElementsByTagName('button')[0];
+
+// const el = document.querySelector('#el');
+// const isi = document.querySelector('#isi');
+// const tempat = document.querySelector('button');
+
+// klik.addEventListener('click', function () {
+//   const elBaru = document.createElement(el.value);
+//   const txtBaru = document.createTextNode(isi.value);
+
+//   elBaru.appendChild(txtBaru);
+
+//   tempat.appendChild(elBaru);
+
+//   alert('ok');
+// });
+
+// klik.addEventListener('click', function () {
+//   const elBaru = document.createElement(el.value);
+//   const txtBaru = document.createTextNode(el.value);
+//   elBaru.appendChild(txtBaru);
+
+//   //tempat
+//   const parent = document.querySelector('body');
+
+//   //eksekusi
+//   parent.insertBefore(elBaru, isi);
+// });
+
+// const parent = document.querySelector('#b ul');
+// const anak = document.querySelector('#ubah');
+
+// const elBaru = document.createElement('h1');
+// const txtBaru = document.createTextNode('Yahya ne');
+
+// elBaru.appendChild(txtBaru);
+
+// parent.replaceChild(elBaru, anak);
