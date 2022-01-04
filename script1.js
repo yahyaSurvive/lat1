b498630873945f04e7f2c9a88a15b6332a193fcd
yahyaSurvive <putuyahya5566@gmail.com>
2022-01-04 22:35:22 +0800

    2 files modified

--- script1.js
+++ script1.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
   $('#btnUmum').click(function () {
     $('#el').toggle(1000);
   });
+
+  $('#hapus').click(function (e) {
+    // $(e).parent().remove();
+    $('#hapus').parent().remove();
+  });
 });
